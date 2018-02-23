4198a55ff19e7a3adf2afd11b84a80c22882687c
Adam Jordanek <dotevo@gmail.com>
2018-02-23 08:33:13 +0100

    Part 3

--- language/eo/PUBLIC.php
+++ language/eo/PUBLIC.php
@@ -58,24 +58,24 @@ $LNG['loginPassword']				= 'Pasvorto';
 $LNG['loginButton']					= 'Ensaluti';
 $LNG['loginInfo']					= 'Per ensaluto mi akceptas %s.';
 $LNG['loginWelcome']				= 'Bonvenon en %s';
-$LNG['loginServerDesc']				= '%s is a space strategy game with hundreds of players playing together <strong>at the same time</strong> trying to be the best. Everything what you need to the play is a Standard Browser.';
+$LNG['loginServerDesc']				= '%s estas strategia ludo kun centoj da ludantoj ludantaj <strong> samtempe </ strong> provante esti la plej bona.';
 
 // Register
 $LNG['registerFacebookAccount']		= 'Fejsbuka konto';
 $LNG['registerUsername']			= 'Uzantnomo';
 $LNG['registerUsernameDesc']		= 'La uzantnomo povas enhavi 3-25 signojn (nombrojn, leterojn, substrekoj kaj spacoj)';
 $LNG['registerPassword']			= 'Pasvorto';
-$LNG['registerPasswordDesc']		= 'Minimume 8 signoj';
+$LNG['registerPasswordDesc']		= 'Minimume %s signoj';
 $LNG['registerPasswordReplay']		= 'Ripetpasvorto';
 $LNG['registerPasswordReplayDesc']	= 'Enskribu la saman pasvorton';
 $LNG['registerEmail']				= 'Retpoŝto';
 $LNG['registerEmailDesc']			= 'Enskribu vian retpoŝtan adreson!';
-$LNG['registerEmailReplay']			= 'Ree Retpoŝto';
+$LNG['registerEmailReplay']			= 'Ripetu retpoŝton';
 $LNG['registerEmailReplayDesc']		= 'Reenskribu vian retpoŝtan adreson!';
 $LNG['registerLanguage']			= 'Lingvo';
-$LNG['registerReferral']			= 'Advertised by:';
-$LNG['registerCaptcha']				= 'Security code';
-$LNG['registerCaptchaDesc']			= 'Enskribu signojn el suba bildo. Kaza sentiveco ne gravas';
+$LNG['registerReferral']			= 'Rekomendita de:';
+$LNG['registerCaptcha']				= 'Sekurkodo';
+$LNG['registerCaptchaDesc']			= 'Enskribu signojn el suba bildo. Usklecoblinda.';
 $LNG['registerCaptchaReload']		= 'Reŝargi.';
 $LNG['registerRules']				= 'Reguloj';
 $LNG['registerRulesDesc']			= 'Mi akceptas %s';
@@ -87,25 +87,25 @@ $LNG['registerErrorUniClosed']		= 'La registro estas fermita en ĉi tiu universo
 $LNG['registerErrorUsernameEmpty']	= 'Enskribu uzantnomon!';
 $LNG['registerErrorUsernameChar']	= 'Via uzantnomo devas enhavi nur nombrojn, leterojn, spacojn, _, -, . !';
 $LNG['registerErrorUsernameExist']	= 'Uzantnomo estas okupita!';
-$LNG['registerErrorPasswordLength']	= 'The password must be at least 6 characters long!';
-$LNG['registerErrorPasswordSame']	= 'Entering 2 different Passwords!';
-$LNG['registerErrorMailEmpty']		= 'You must specify an E-Mail address!';
-$LNG['registerErrorMailInvalid']	= 'Invalid E-Mail address!';
-$LNG['registerErrorMailSame']		= 'You have specified 2 different email addresses!';
-$LNG['registerErrorMailExist']		= 'The E-Mail address is already registered!';
-$LNG['registerErrorRules']			= 'You have to accept the rules!';
-$LNG['registerErrorCaptcha']		= 'The security code is incorrect!';
+$LNG['registerErrorPasswordLength']	= 'The password must be at least %s characters long!';
+$LNG['registerErrorPasswordSame']	= 'Vi donis du malsamajn pasvortojn. Bonvolu reprovi!';
+$LNG['registerErrorMailEmpty']		= 'Vi devas doni retpoŝtadreson!';
+$LNG['registerErrorMailInvalid']	= 'Nevalida retpoŝtadreso!';
+$LNG['registerErrorMailSame']		= 'Vi donis du malsamajn retpoŝtadresojn. Bonvolu reprovi!';
+$LNG['registerErrorMailExist']		= 'retpoŝtadreso estas okupita!';
+$LNG['registerErrorRules']			= 'Vi devas akcepti la regulojn!';
+$LNG['registerErrorCaptcha']		= 'Nevalida sekurkodo!';
 
 $LNG['registerMailVertifyTitle']	= 'Activation of registration on the game: %s';
-$LNG['registerMailVertifyError']	= 'Failed to send mail: %s';
+$LNG['registerMailVertifyError']	= 'Ne eblis sendi mesaĝon: %s';
 
-$LNG['registerMailCompleteTitle']	= 'Welcome to %s!';
+$LNG['registerMailCompleteTitle']	= 'Bonvenon al %s!';
 
 $LNG['registerSendComplete']		= 'Thank you for the registration. You must check your email (Attention with the SPAM) with further information.';
 
-$LNG['registerWelcomePMSenderName']	= 'Administrator';
-$LNG['registerWelcomePMSubject']	= 'Welcome';
-$LNG['registerWelcomePMText']		= 'Welcome to %s! First build a solar power plant,because energy is needed for the production of raw materials. To build one, left click in the menu on "buildings". Then build the 4th Building from the top. </br> When you have energy, you can begin to build mines. Go to buildings on the menu and build a metal mine, followed by a crystal mine. </br></br>In order to be able to build ships you need to have a shipyard. To see what is needed to unlock that building you can take a look at "Technologies" at the left menu.</br>If you more questions you can look at the beginnersguide, our forum or open a support ticket. </br></br> The team wishes you much fun exploring the universe!';
+$LNG['registerWelcomePMSenderName']	= 'Administranto';
+$LNG['registerWelcomePMSubject']	= 'Bonvenon';
+$LNG['registerWelcomePMText']		= 'Bonvenon al %s! Unue konstruu Sunenergian Elektrejo, ĉar energio estas bezonata por la produktado de unuaj materioj. Por konstrui unu, maldekstre alklaku la menuon sur "konstruaĵoj". Poste konstruu la 4a Konstruaĵon el la supro. Kiam vi havos energion, vi povos komenci konstrui minojn. Iru al konstruaĵoj en la menuo kaj konstruu metalan minon, sekvitan de kristalo. </br> </br> Por konstrui ŝipojn, vi devas havi ŝipkonstruejon. Por vidi, kio bezonas por konstrui tiun konstruaĵon, vi povas rigardi "Teknologioj"n maldekstre.</br>';
 
 //Vertify
 
@@ -114,13 +114,13 @@ $LNG['vertifyAdminMessage']			= 'The username "%s" has enabled!';
 
 
 //lostpassword
-$LNG['passwordInfo']				= 'If you have forgotten your password, you must specify the user name and the E-Mail address that you have entered in your account.';
-$LNG['passwordUsername']			= 'Username';
-$LNG['passwordMail']				= 'E-Mail';
-$LNG['passwordCaptcha']				= 'Security code';
-$LNG['passwordSubmit']				= 'Submit';
-$LNG['passwordErrorUsernameEmpty']	= 'You have no specified the Username!';
-$LNG['passwordErrorMailEmpty']		= 'You have specified an unknown email address!';
+$LNG['passwordInfo']				= 'Se vi forgesis vian pasvorton, vi devas specifi la uzantnomon kaj la retpoŝtan adreson, kiujn vi enigis.';
+$LNG['passwordUsername']			= 'Uzantnomo';
+$LNG['passwordMail']				= 'Retpoŝtadreso';
+$LNG['passwordCaptcha']				= 'Sekurkodo';
+$LNG['passwordSubmit']				= 'Sendi';
+$LNG['passwordErrorUsernameEmpty']	= 'Uzantnomo mankas!';
+$LNG['passwordErrorMailEmpty']		= 'Nekonata retpoŝtadreso!';
 $LNG['passwordErrorUnknown']		= 'It could not be found with the account data.';
 $LNG['passwordErrorOnePerDay']		= 'Password for this user account in the last 24 hours previously requested. You can only request the password for this user account in  next 24 hours. ';
 
@@ -131,23 +131,23 @@ $LNG['passwordValidInValid']		= 'Invalid Request!';
 $LNG['passwordChangedMailSend']		= 'You will receive an email shortly with your new password.';
 $LNG['passwordChangedMailTitle']	= 'New password on the game: %s';
 
-$LNG['passwordBack']				= 'Back';
-$LNG['passwordNext']				= 'Next';
+$LNG['passwordBack']				= 'Malsekva';
+$LNG['passwordNext']				= 'Sekva';
 
 //case default
 
-$LNG['login_error_1']				= 'Wrong username/password!';
+$LNG['login_error_1']				= 'Malĝusta usantnomo/pasvorto!';
 $LNG['login_error_2']				= 'Someone has logged in from a different PC in your account, or your IP is different!';
 $LNG['login_error_3']				= 'Your session has expired!';
 $LNG['login_error_4']				= 'There was an error in external authorization, please try again!';
 
 //Rules
-$LNG['rulesHeader']					= 'Rules';
+$LNG['rulesHeader']					= 'Reguloj';
 
 //NEWS
-$LNG['news_overview']				= 'News';
+$LNG['news_overview']				= 'Novaĵoj';
 $LNG['news_from']					= 'On %s by %s';
-$LNG['news_does_not_exist']			= 'No news available!';
+$LNG['news_does_not_exist']			= 'Neniu novaĵo!';
 
 //Impressum
 $LNG['disclamerLabelAddress']		= 'Game Address:';
